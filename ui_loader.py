c82d0ef43c8903ca85aef8d1b140527f1bc4981d
Josh <challo4joshdj@gmail.com>
2024-02-29 07:34:16 +1100

    Update ui_loader.py

--- ui_loader.py
+++ ui_loader.py
@@ -42,6 +42,16 @@ def init_ui(
     
 
 
+
+
+    # Main menu page
+
+    def main_menu_join_clicked():
+        manager.set_page("game_join")
+
+    def main_menu_create_clicked():
+        manager.set_page("game_create")
+
     _page = UiPage("main")
     _page.add_element(Label(
         identifier="label_1",
@@ -58,22 +68,51 @@ def init_ui(
         font=font_secondary
     ))
 
-    _button_1 = Container(
-        identifier="button_1",
-        rect=pygame.Rect(10, 30, 80, 10))
-    _button_1_left = _button_1.add_child(Element(
+    _button_panel = Container(
+        identifier="button_container",
+        rect=pygame.Rect(20, 75, 0, 0)
+    )
+
+    _button = Container(
+        identifier="button_join_game",
+        rect=pygame.Rect(0, 0, 100, 20))
+    _button_left = _button.add_child(Element(
         "button_1_left",
-        rect=pygame.Rect(0,0,2,10),
+        rect=pygame.Rect(0, 5, 2, 10),
         renderer=ElementRenderer()
     ))
-    _button_1_left.renderer.color = (230, 230, 230)
-    _button_1_text = _button_1.add_child(Label(
+    _button_left.renderer.color = (230, 230, 230)
+    _button.add_child(Label(
         identifier="button_1_text",
-        position=( 5, 2 ),
+        position=( 6, 8 ),
         text="Join A Game",
         text_alignment=(-1, -1),
         font=font_main
     ))
-    _page.add_element(_button_1)
+    _button.can_click = True
+    _button.on_click = main_menu_join_clicked
+    _button_panel.add_child(_button)
+
+    _button = Container(
+        identifier="button_create_game",
+        rect=pygame.Rect(0, 30, 100, 20))
+    _button_left = _button.add_child(Element(
+        "button_2_left",
+        rect=pygame.Rect(0, 5, 2, 10),
+        renderer=ElementRenderer()
+    ))
+    _button_left.renderer.color = (230, 230, 230)
+    _button.add_child(Label(
+        identifier="button_2_text",
+        position=( 6, 8 ),
+        text="Create New Game",
+        text_alignment=(-1, -1),
+        font=font_main
+    ))
+    _button.can_click = True
+    _button.on_click = main_menu_create_clicked
+    _button_panel.add_child(_button)
+
+    _page.add_element(_button_panel)
 
     manager.add_page(_page)
